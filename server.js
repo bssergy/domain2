d5b68749c53b395bf818f10d863bfc5213af71c4
bssergy <bssergy@gmail.com>
2015-09-30 20:53:07 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ success: true }));
+}).listen(3000, function() {
+    console.log('Server is listening on port 3000');
+});
