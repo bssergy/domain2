5e4ac9dc79cae893c95e9fafd8a8a41603a2f3be
Sergij Brao <sbrao@ancestry.com>
2015-10-01 10:57:46 +0300

    Changed port

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var fs = require('fs');
 http.createServer(function(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({ success: true }));
-}).listen(3000, function() {
+}).listen(8080, function() {
     console.log('Server is listening on port 3000');
 });
