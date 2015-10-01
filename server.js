3d3f28bffbff182ebe2f808a80e5f20035d86e43
Sergij Brao <sbrao@ancestry.com>
2015-10-01 17:42:13 +0300

    Added cross origin headers

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = require('express')();
 
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'x-access-token');
+    res.setHeader('Access-Control-Allow-Headers', 'x-access-token,Content-Type');
     if (req.method == 'OPTIONS') {
         return res.status(204).end();
     }
